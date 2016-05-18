5632b1860e8b1237877d79c9d944bad8094fe3d9
Robert Boloc <robertboloc@gmail.com>
2016-05-18 22:13:29 +0100

    Added initial banner

--- js/content.js
+++ js/content.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', chrome.extension.getURL('/html/content.html'), true);
+xmlHttp.send(null);
+
+xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 || xmlHttp.readyState == 'complete') {
+        var inject = document.createElement('div');
+        inject.innerHTML = xmlHttp.responseText
+        document.body.insertBefore(inject, document.body.firstChild);
+    }
+}
